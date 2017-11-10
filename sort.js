78496c8b796894a3e642b5be841bece1c55786be
Victor Wu <victor.wu.1@vanderbilt.edu>
2017-11-10 11:36:26 -0500

    selection sort done

--- sort.js
+++ sort.js
@@ -22,16 +22,17 @@ document.addEventListener('DOMContentLoaded', ()=> {
   // debugger
   resetData();
   drawBars(data);
-  selectionSort(data);
+  // selectionSort(data);
+  betterSelectionSort(data);
 })
 
 function resetData() {
-  length = 40;
+  length = 50;
 
   data = [];
 
   for (var i = 0; i < length; i++) {
-    data.push( { num: Math.ceil((Math.random() * length)) } )
+    data.push( { num: Math.ceil((Math.random() * length)), color: "gray" } )
   }
 }
 
@@ -68,8 +69,8 @@ function drawBars(data) {
   rects.attr("fill", "gray");
 }
 
-function redrawBars(data) {
-  var rects = svg.selectAll("rect").data(data).transition().duration(100);
+function redrawBars() {
+  var rects = svg.selectAll("rect").data(data).transition().duration(50);
 
   rects.attr("x", function(el, i) {
     return i * (w / data.length);
@@ -87,34 +88,74 @@ function redrawBars(data) {
     return scale(el.num);
   });
 
-  rects.attr("fill", "gray");
+  rects.attr("fill", function(el, i) {
+    return el.color;
+  });
 }
 
-function selectionSort(data) {
-  var swap = false;
-  var min;
-  for (var i = 0; i < data.length - 1; i++) {
 
-    min_idx = i;
+// var i = 0
+// function selectionSort(data) {
+//   var swap = false;
+//   var min_idx = i;
+//
+//   setTimeout(function() {
+//
+//     if (i < data.length) {
+//       for (var j = i + 1; j < data.length; j++) {
+//         // debugger
+//         data[j].color = "red";
+//         if (data[j].num < data[min_idx].num) {
+//           swap = true;
+//           min_idx = j;
+//         }
+//         data[j].color = "gray"
+//       }
+//       if (swap) {
+//         swapEl(min_idx, i);
+//       }
+//
+//       redrawBars();
+//       i++;
+//       selectionSort(data);
+//     }
+//   }, 100);
+// }
 
-    for (var j = i + 1; j < data.length; j++) {
-      // debugger
-      if (data[j].num < data[min_idx].num) {
+
+function betterSelectionSort(data) {
+  var swap, i = 0, j = i + 1, min_idx = i
+  swap = false;
+
+  sort = setInterval(function() {
+    if (j >= data.length) {
+      min_idx = i;
+      j = i + 1;
+    }
+
+    if (i < data.length - 1) {
+      data[j].color = "red";
+      if (data[j].num <= data[min_idx].num) {
         swap = true;
         min_idx = j;
       }
-
+      if (j === data.length - 1) {
+        if (swap) {
+          data[min_idx].color = "gray";
+          swapEl(min_idx, i);
+        }
+        data[i].color = "blue";
+        swap = false;
+        i++;
+      }
+      redrawBars();
+      data[j].color = "gray";
+    } else {
+      data[i].color = "blue";
+      redrawBars();
+      clearInterval(sort);
     }
-    if (swap) {
-      swapEl(min_idx, i);
-    }
 
-    myTimeout();
-    // setTimeout(()=>{}, 50000);
-  }
-
-}
-
-function myTimeout() {
-  setTimeout(function() {redrawBars(data)}, 1000);
+    j++;
+  }, 20);
 }
